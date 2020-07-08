6ac4e48acf191a9b0215bfd9f232d84b4aa7903d
John Santos <johnsousasantos98@gmail.com>
2020-07-08 05:48:14 -0300

    Adicionando os arquivos iniciais do projeto

---
+++ Script.js
@@ -0,0 +1,6 @@
+//Navbar dropdown
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instancesDropdown = M.Dropdown.init(elemsDropdown,{
+    coverTrigger: false,
+    hover: false
+});
